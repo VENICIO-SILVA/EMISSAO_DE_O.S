fce96f35708b78326ad1a7e1f47f014dec50a71b
venicio da silva ferreira <veniciodasilvaferreira9@gmail.com>
2025-09-22 22:35:26 -0300

    conexao com banco de dados

--- src/main/java/br/com/project/conexao/Dao.java
+++ src/main/java/br/com/project/conexao/Dao.java
@@ -1,7 +1,15 @@
 package br.com.project.conexao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Dao {
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Ordem_De_Servico");
+        EntityManager em = emf.createEntityManager();
 
+        em.close();
+        emf.close();
     }
 }
