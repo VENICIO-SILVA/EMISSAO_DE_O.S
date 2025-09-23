fca472244e8adeb9d7c45d76c343c817c236cfc1
venicio da silva ferreira <veniciodasilvaferreira9@gmail.com>
2025-09-23 20:04:16 -0300

    Classe criada para Realizar consulta de usuario para login na tela de login

---
+++ src/main/java/br/com/project/usuarios/Usuarios.java
@@ -0,0 +1,55 @@
+package br.com.project.usuarios;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuarios {
+    @Id
+    private long IdUser;
+
+    private String usuario;
+    private String telefone;
+    private String gmail;
+    private String senha;
+
+    public long getIdUser() {
+        return IdUser;
+    }
+
+    public void setIdUser(long idUser) {
+        IdUser = idUser;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getGmail() {
+        return gmail;
+    }
+
+    public void setGmail(String gmail) {
+        this.gmail = gmail;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
