e25c563480cf497cc189bd697d798ec55bc95e5a
venicio da silva ferreira <veniciodasilvaferreira9@gmail.com>
2025-09-27 14:01:06 -0300

    Classe/entidade para clientes do banco de dados

---
+++ src/main/java/br/com/project/usuarios/Clientes.java
@@ -0,0 +1,75 @@
+package br.com.project.usuarios;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Clientes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCli;
+
+    @Column(nullable = true)
+    private String nome;
+
+    @Column(nullable = true)
+    private String Endereco;
+
+    @Column(nullable = true)
+    private String telefone;
+
+    @Column(unique = true, nullable = true)
+    private String gmail;
+
+    public Clientes() {
+
+    }
+
+    public Clientes(String nome, String Endereco, String telefone, String gmail) {
+        super();
+        this.nome = nome;
+        this.Endereco = Endereco;
+        this.telefone = telefone;
+        this.gmail = gmail;
+    }
+
+    public Long getIdCli() {
+        return idCli;
+    }
+
+    public void setIdCli(Long idCli) {
+        this.idCli = idCli;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return Endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        Endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getGmail() {
+        return gmail;
+    }
+
+    public void setGmail(String gmail) {
+        this.gmail = gmail;
+    }
+}
