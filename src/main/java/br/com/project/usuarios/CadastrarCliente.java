99715bde2ae1f19d9bb58d01b8a00b2f2cda6ed0
venicio da silva ferreira <veniciodasilvaferreira9@gmail.com>
2025-09-27 15:47:41 -0300

    adicionei os retornos e dentro do "Catch" caso o usuario insira um email igual ao que ja existe dentro do banco de dados o proprio JPA notifica entao tambem coloquei uma tela mostrando e voltando para o cliente digitar um Gmail diferente

--- src/main/java/br/com/project/usuarios/CadastrarCliente.java
+++ src/main/java/br/com/project/usuarios/CadastrarCliente.java
@@ -14,9 +14,10 @@ public class CadastrarCliente {
                 JOptionPane.showMessageDialog(null, "Preencha o campo gmail");
                 //como o email é unico no banco de dados se ele vier vazio do campo de inserção ja faço uma verifação para
                 //mostrar um erro com letras estranhas na tela ou erro durante a execução
+                return InserirCLiente;
                 if (nome.getText().isEmpty() && telefone.getText().isEmpty() && endereco.getText().isEmpty()) {
                     JOptionPane.showMessageDialog(null, "Preencha os campos");
-
+                    return InserirCLiente;
                 }
             } else {
 
