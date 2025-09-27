635d38202d7cc1b1787ad2e31fd08f6879db703d
venicio da silva ferreira <veniciodasilvaferreira9@gmail.com>
2025-09-27 15:46:23 -0300

    Logica para caso a pessoa clique em cadastrar os cliente com os campos vazios apresentar uma mensagem e o cliente preencher novamente os campos da mesma forma o email

--- src/main/java/br/com/project/usuarios/CadastrarCliente.java
+++ src/main/java/br/com/project/usuarios/CadastrarCliente.java
@@ -5,29 +5,36 @@ import br.com.project.conexao.Dao;
 import javax.swing.*;
 
 public class CadastrarCliente {
-   public Clientes Cadastrar(JTextField nome,JTextField telefone, JTextField endereco, JTextField gmail){
+    public Clientes Cadastrar(JTextField nome, JTextField telefone, JTextField endereco, JTextField gmail) {
         Dao conexao = new Dao();
         conexao.IniciarConexao();
         Clientes InserirCLiente = new Clientes();
         try {
-            if(gmail.getText().isEmpty()){
-                JOptionPane.showMessageDialog(null,"Preencha o campo gmail");
-            }//como o email é unico no banco de dados se ele vier vazio do campo de inserção ja faço uma verifação para
-            //mostrar um erro com letras estranhas na tela ou erro durante a execução
+            if (gmail.getText().isEmpty() == true) {
+                JOptionPane.showMessageDialog(null, "Preencha o campo gmail");
+                //como o email é unico no banco de dados se ele vier vazio do campo de inserção ja faço uma verifação para
+                //mostrar um erro com letras estranhas na tela ou erro durante a execução
+                if (nome.getText().isEmpty() && telefone.getText().isEmpty() && endereco.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Preencha os campos");
 
-        InserirCLiente.setNome(nome.getText());
-        InserirCLiente.setEndereco(endereco.getText());
-        InserirCLiente.setTelefone(telefone.getText());
-        InserirCLiente.setGmail(gmail.getText());
-        conexao.em.getTransaction().begin();
-        conexao.em.persist(InserirCLiente);
-        conexao.em.getTransaction().commit();
+                }
+            } else {
 
-        }catch(Exception e){
-            JOptionPane.showMessageDialog(null,"Erro de Insert");
-        }finally {
+                InserirCLiente.setNome(nome.getText());
+                InserirCLiente.setEndereco(endereco.getText());
+                InserirCLiente.setTelefone(telefone.getText());
+                InserirCLiente.setGmail(gmail.getText());
+                conexao.em.getTransaction().begin();
+                conexao.em.persist(InserirCLiente);
+                conexao.em.getTransaction().commit();
+            }
+
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Seu Gmail nao pode ser igual ao de Alguem ja Cadastrado");
+        } finally {
             conexao.FecharConexao();
         }
-       return InserirCLiente;
-   }
+        return InserirCLiente;
+    }
 }
