a5da3f27a6ab9fb6dc03e4eba314de1c5640cc9d
venicio da silva ferreira <veniciodasilvaferreira9@gmail.com>
2025-09-28 10:56:28 -0300

    Essa classe segue a mesma logica do cadastro de cliente!

---
+++ src/main/java/br/com/project/usuarios/CadastrarAdmin.java
@@ -0,0 +1,41 @@
+package br.com.project.usuarios;
+
+import br.com.project.conexao.Dao;
+
+import javax.swing.*;
+
+public class CadastrarAdmin {
+    public Usuarios Cadastrar(JTextField Usuario, JTextField Senha, JTextField telefone, JTextField gmail) {
+        Dao conexao = new Dao();
+        conexao.IniciarConexao();
+        Usuarios InsertUsuario = new Usuarios();
+        try {
+            //todo organizar quando cada Alerta deve aparecer porque quando os campos estao vazios esta rodando o do email tambem
+            if (gmail.getText().isEmpty() == true) {
+                JOptionPane.showMessageDialog(null, "Preencha o campo gmail");
+                //como o email é unico no banco de dados se ele vier vazio do campo de inserção ja faço uma verifação para
+                //mostrar um erro com letras estranhas na tela ou erro durante a execução
+                if (Usuario.getText().isEmpty() && Senha.getText().isEmpty() && telefone.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Preencha os campos");
+                    return InsertUsuario;
+                }
+            } else {
+
+                InsertUsuario.setUsuario(Usuario.getText());
+                InsertUsuario.setSenha(Senha.getText());
+                InsertUsuario.setTelefone(telefone.getText());
+                InsertUsuario.setGmail(gmail.getText());
+                conexao.em.getTransaction().begin();
+                conexao.em.persist(InsertUsuario);
+                conexao.em.getTransaction().commit();
+            }
+
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Seu Gmail nao pode ser igual ao de Alguem ja Cadastrado");
+        } finally {
+            conexao.FecharConexao();
+        }
+        return InsertUsuario;
+    }
+}
