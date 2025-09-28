390f03c6106ce0ba34f704ce9deccaec22ab42af
venicio da silva ferreira <veniciodasilvaferreira9@gmail.com>
2025-09-28 17:44:17 -0300

    Alteração para nao fechar todo o progama por completo

--- src/main/java/br/com/project/telas/TelaCadastroAdmin.java
+++ src/main/java/br/com/project/telas/TelaCadastroAdmin.java
@@ -44,7 +44,7 @@ public class TelaCadastroAdmin extends javax.swing.JFrame {
         LbTitulo = new javax.swing.JLabel();
         BtCadastrar = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Cadastrar Cliente");
 
         LbUsuario.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
