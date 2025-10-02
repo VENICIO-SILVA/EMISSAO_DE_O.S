e0cd8d04a6c0bfe2e4977167098c769f77f7931f
venicio da silva ferreira <veniciodasilvaferreira9@gmail.com>
2025-10-01 21:06:15 -0300

    Aqui configurei a tela de login para o Login como Cliente -1 necessario mudar para public Object pois esta apenas aceitando retorno como Usuario/classe de ADM.
    2-comentario
    
    3-Como as consultas de cliente e adms sao de tabelas distinta e necessario 2 consulta/Query no banco de dados
    
    4-para cada consulta realizar uma verificação de if caso nao encontre pelo perfil "admin" e "cliente" retornando seus devidos dados
    
    5- pelo fato do retorno ser Object vai aceitar de ambas classes instanciadas na classe/LoginAdm

--- src/main/java/br/com/project/usuarios/LoginAdmin.java
+++ src/main/java/br/com/project/usuarios/LoginAdmin.java
@@ -5,17 +5,19 @@ import jakarta.persistence.TypedQuery;
 
 import javax.swing.*;
 import java.util.List;
+import java.util.Objects;
 
 public class LoginAdmin {
-    public Usuarios Login(JTextField login, JPasswordField senha) {
+    public Object Login(JTextField login, JPasswordField senha) {
         Dao conexao = new Dao();
         conexao.IniciarConexao();
         Usuarios user = new Usuarios();
+        Clientes clientes = new Clientes();
         try {
-            boolean valido = true;
+            //os primeiros if verificam os campos
             if (login.getText().isEmpty() && senha.getText().isEmpty()) {
                 JOptionPane.showMessageDialog(null, "E obrigatório Informar os dados para login");
-                valido = false;
+
                 return null;
             }
 
@@ -34,27 +36,53 @@ public class LoginAdmin {
                 TypedQuery<Usuarios> query = conexao.em.createQuery(jpql, Usuarios.class);
                 query.setParameter("login", login.getText());
                 query.setParameter("senha", new String(senha.getPassword()));
-                List<Usuarios> list = query.getResultList();
-                user = query.getSingleResult();
-                if (list.isEmpty()) {
-                    JOptionPane.showMessageDialog(null, "Usuário não existe");
-                    return null;
+                List<Usuarios> listUsuarios = query.getResultList();
+
+                if (!listUsuarios.isEmpty()) {
+                    user = listUsuarios.get(0);
+
+
+                    if ("admin".equalsIgnoreCase(user.getPerfil())) {
+                        JOptionPane.showMessageDialog(null, "Login realizado com sucesso como ADMIN!");
+                        return user;
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Usuário encontrado mas não é ADMIN");
+                        return null;
+                    }
                 }
-                if ("admin".equals(user.getPerfil())){
-                    JOptionPane.showMessageDialog(null,"Login realizado com sucesso!");
-                    return user;
-                }else{
-                    JOptionPane.showMessageDialog(null,"Este Usuario nao é ADMIN");
-                    return null;
+
+
+                //segunda consulta do banco de dados
+                String jpql2 = " select u from Clientes u where u.gmail = :login and u.senha = :senha";
+                TypedQuery<Clientes> query2 = conexao.em.createQuery(jpql2, Clientes.class);
+                query2.setParameter("login", login.getText());
+                query2.setParameter("senha", new String(senha.getPassword()));
+                List<Clientes> ListClientes = query2.getResultList();
+                //
+                if (!ListClientes.isEmpty()) {
+                    clientes = ListClientes.get(0);
+
+
+                    if ("Cliente".equals(clientes.getPerfil())) {
+                        JOptionPane.showMessageDialog(null, "Login realizado com sucesso como Cliente!");
+                        return clientes;
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Usuário encontrado mas não é ADMIN");
+                        return null;
+                    }
                 }
+
+                JOptionPane.showMessageDialog(null, "Este Usuario nao é ADMIN");
+                return null;
             }
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null,"Este Usuario  nao existe");
-            return null;
+
         } finally {
             conexao.FecharConexao();
         }
+        return null;
     }
 }
+
 
 
