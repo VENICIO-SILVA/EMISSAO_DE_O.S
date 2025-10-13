550b0aeaadf9bfbdc4cdcdf54475ceace0696cd1
Desktop <veniciodasilvaferreira9@gmail.com>
2025-10-12 22:33:19 -0300

    correção merge

--- src/main/java/br/com/project/domain/OrdensDeServico.java
+++ src/main/java/br/com/project/domain/OrdensDeServico.java
@@ -1,10 +1,32 @@
 package br.com.project.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+import java.util.Timer;
 
 @Entity
-@Table(name = os)
+@Table(name = "os")
 public class OrdensDeServico {
+
+    @Id
+    private int os;
+
+    private Timestamp data_os;
+
+    private String equipamento;
+
+    private String defeito;
+
+    private String servico;
+
+    private String tecnico;
+
+    private Double valor;
+
+    @ManyToOne
+    @JoinColumn(name = "idcli")
+    private Clientes cliente;
+
 
 }
