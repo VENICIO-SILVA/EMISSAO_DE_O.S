8501f9dcad8a922f8a492067d1982008c1fbf14b
Desktop <veniciodasilvaferreira9@gmail.com>
2025-10-15 11:59:52 -0300

    Entidade OS

--- src/main/java/br/com/project/domain/OrdensDeServico.java
+++ src/main/java/br/com/project/domain/OrdensDeServico.java
@@ -28,5 +28,67 @@ public class OrdensDeServico {
     @JoinColumn(name = "idcli")
     private Clientes cliente;
 
+    public int getOs() {
+        return os;
+    }
 
+    public void setOs(int os) {
+        this.os = os;
+    }
+
+    public Timestamp getData_os() {
+        return data_os;
+    }
+
+    public void setData_os(Timestamp data_os) {
+        this.data_os = data_os;
+    }
+
+    public String getEquipamento() {
+        return equipamento;
+    }
+
+    public void setEquipamento(String equipamento) {
+        this.equipamento = equipamento;
+    }
+
+    public String getDefeito() {
+        return defeito;
+    }
+
+    public void setDefeito(String defeito) {
+        this.defeito = defeito;
+    }
+
+    public String getServico() {
+        return servico;
+    }
+
+    public void setServico(String servico) {
+        this.servico = servico;
+    }
+
+    public String getTecnico() {
+        return tecnico;
+    }
+
+    public void setTecnico(String tecnico) {
+        this.tecnico = tecnico;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Clientes getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Clientes cliente) {
+        this.cliente = cliente;
+    }
 }
