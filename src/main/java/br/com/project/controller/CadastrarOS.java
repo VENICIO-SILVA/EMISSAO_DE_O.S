b88449d631963175edc6c2428aa65265bcf69a7d
Desktop <veniciodasilvaferreira9@gmail.com>
2025-10-15 11:59:19 -0300

    Metodo para consulta do cliente para vincular com a chave estrangeira da coluna OS. Obs em teste

--- src/main/java/br/com/project/controller/CadastrarOS.java
+++ src/main/java/br/com/project/controller/CadastrarOS.java
@@ -1,6 +1,38 @@
 package br.com.project.controller;
 
+import br.com.project.dao.Dao;
 import br.com.project.domain.OrdensDeServico;
 
+import javax.swing.*;
+import java.sql.Timestamp;
+
 public class CadastrarOS {
+ public OrdensDeServico cadastrarOS(JTextField Numeros, JTextField DataOs, JTextField Equipamento, JTextField servico, JTextField defeito,JTextField tecnico, JTextField valor){
+     Dao conexao = new Dao();
+     conexao.IniciarConexao();
+     OrdensDeServico InsertOs = new OrdensDeServico();
+     try {
+         InsertOs.setOs(Integer.parseInt(Numeros.getText()));
+
+         InsertOs.setData_os(Timestamp.valueOf(DataOs.getText()));
+
+         InsertOs.setEquipamento(Equipamento.getText());
+
+         InsertOs.setServico(servico.getText());
+
+         InsertOs.setDefeito(defeito.getText());
+
+         InsertOs.setTecnico(tecnico.getText());
+
+         conexao.em.getTransaction().begin();
+         conexao.em.persist(InsertOs);
+         conexao.em.getTransaction().commit();
+
+     }catch (Exception e ){
+         JOptionPane.showMessageDialog(null, "Erro de conexao" + e.getMessage());
+     }finally {
+         conexao.FecharConexao();
+     }
+     return InsertOs;
+ }
 }
