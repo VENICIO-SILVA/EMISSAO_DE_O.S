656a7f6961cba811deaa1d9a59c3502392891dee
Desktop <veniciodasilvaferreira9@gmail.com>
2025-10-31 18:14:18 -0300

    Classe controler onde converte o arquivos o jrxml para tipo jasper, e por fim gerar o PDF

---
+++ src/main/java/br/com/project/controller/GerarRelatorio.java
@@ -0,0 +1,36 @@
+package br.com.project.controller;
+
+import br.com.project.dao.Dao;
+import net.sf.jasperreports.engine.*;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GerarRelatorio {
+    public static void gerarRelatorio() {
+    Dao conexao = new Dao();
+        try {
+            InputStream caminho = GerarRelatorio.class.getResourceAsStream("/relatorios/BaseRelatorioO.S.jrxml");
+
+            if (caminho == null) {
+                System.out.println("⚠️ Arquivo .jrxml não encontrado!");
+                return;
+            }
+
+            JasperReport report = JasperCompileManager.compileReport(caminho);
+
+            Map<String, Object> parametros = new HashMap<>();
+            parametros.put("TITULO", "Relatório de Serviços");
+
+
+            JasperPrint print = JasperFillManager.fillReport(report, parametros, conexao.getConnection());
+
+            JasperExportManager.exportReportToPdfFile(print, "C:\\Users\\Venicio\\Desktop\\RELATORIOos\\Relatorio_de_Serviços.pdf");
+            System.out.println("✅ Relatório gerado com sucesso!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
