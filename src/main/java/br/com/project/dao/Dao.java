11969c741eaaeff4520d05cb663a3fd700879e8a
Desktop <veniciodasilvaferreira9@gmail.com>
2025-10-31 18:12:58 -0300

    //este metodo foi criado pois na classe controller "GerarRelatorio e esperado um "Connection" e nao consegui usar o Dao "emf" e "em"

--- src/main/java/br/com/project/dao/Dao.java
+++ src/main/java/br/com/project/dao/Dao.java
@@ -4,9 +4,15 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Dao {
     private EntityManagerFactory emf;
     public EntityManager em;
+
+    private Connection conn;
+
 
     public Dao() {
 
@@ -31,5 +37,15 @@ public class Dao {
         } catch (Exception e) {
             System.out.println("Erro ao fechar conexão: " + e.getMessage());
         }
+    }
+    //este metodo foi criado pois na classe controller "GerarRelatorio e esperado um "Connection" e nao consegui usar o Dao "emf" e "em"
+    public Connection getConnection() throws Exception {
+        if (conn == null || conn.isClosed()) {
+            String url = "jdbc:mysql://localhost:3306/projetoos";
+            String user = "root";
+            String password = "coxinha220";
+            conn = DriverManager.getConnection(url, user, password);
+        }
+        return conn;
     }
 }
