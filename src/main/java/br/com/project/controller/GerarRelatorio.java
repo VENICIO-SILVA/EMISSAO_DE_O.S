f0f22cd830033ffd62b8c92857f63d15a272a17f
Desktop <veniciodasilvaferreira9@gmail.com>
2025-11-04 21:56:31 -0300

    chore - tarefas

--- src/main/java/br/com/project/controller/GerarRelatorio.java
+++ src/main/java/br/com/project/controller/GerarRelatorio.java
@@ -27,12 +27,14 @@ public class GerarRelatorio {
             //faz a conversao do arquivo encontrado para objeto java para que o Jasper Entenda o arquivo
             JasperReport report = JasperCompileManager.compileReport(LocalArquivo);
 
+            //todo no momento nao tem uso, mas irei utulizar para inserir dados complementares no PDF
             Map<String, Object> parametros = new HashMap<>();
             parametros.put("TITULO", "Relatório de Serviços");
 
 
             JasperPrint print = JasperFillManager.fillReport(report, parametros, conexao.getConnection());
 
+            //todo configurar um meio para quando for relatorio de cliente ou de valores criar o arquivo com o nome de acordo e nao um unico nome
             JasperExportManager.exportReportToPdfFile(print, "C:\\Users\\Venicio\\Desktop\\RELATORIOos\\Relatorio_de_Serviços.pdf");
             System.out.println("✅ Relatório gerado com sucesso!");
 
