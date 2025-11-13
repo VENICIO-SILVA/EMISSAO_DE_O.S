fa9e5d8af2f8b4dc0f91a9b75cbf2374dc422779
Desktop <veniciodasilvaferreira9@gmail.com>
2025-11-12 21:28:25 -0300

    feat - Case para separar o tipo de consulta do relatorio 1- ordens de serviço 2- clientes 3- Valores(Em construção)

--- src/main/java/br/com/project/controller/GerarRelatorio.java
+++ src/main/java/br/com/project/controller/GerarRelatorio.java
@@ -10,61 +10,115 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
- //todo criar outro jrxml e configura manual com o aprendizado e ordem de tags
+
+//todo criar outro jrxml e configura manual com o aprendizado e ordem de tags
 public class GerarRelatorio {
 
-    public static void GerarRelatorio(String caminho, String nomeArquivoSaida,
+    public static void GerarRelatorio(String Tipo_Relatorio, String caminho, String nomeArquivoSaida,
                                       JTextField dataInicio, JTextField dataFim) {
 
         Dao conexao = new Dao();
         InputStream localArquivo = null;
 
+
         try {
             conexao.IniciarConexao();
 
-            // Converte as datas do JTextField
-            DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-            LocalDate dataI = LocalDate.parse(dataInicio.getText(), formatador);
-            LocalDate dataF = LocalDate.parse(dataFim.getText(), formatador);
+            switch (Tipo_Relatorio) {
+                case "Ordens":
+                    // Converte as datas do JTextField
+                    DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                    LocalDate dataI = LocalDate.parse(dataInicio.getText(), formatador);
+                    LocalDate dataF = LocalDate.parse(dataFim.getText(), formatador);
 
 
-            // Confere se há OS no período
-            String jpql = "SELECT COUNT(o) FROM OrdensDeServico o WHERE o.data_os BETWEEN :dataI AND :dataF";
-            TypedQuery<Long> query = conexao.em.createQuery(jpql, Long.class);
-            query.setParameter("dataI", java.sql.Timestamp.valueOf(dataI.atStartOfDay()));
-            query.setParameter("dataF", java.sql.Timestamp.valueOf(dataF.atStartOfDay()));
-            Long total = query.getSingleResult();
+                    // Confere se há OS no período
+                    String jpql = "SELECT COUNT(o) FROM OrdensDeServico o WHERE o.data_os BETWEEN :dataI AND :dataF";
+                    TypedQuery<Long> query = conexao.em.createQuery(jpql, Long.class);
+                    query.setParameter("dataI", java.sql.Timestamp.valueOf(dataI.atStartOfDay()));
+                    query.setParameter("dataF", java.sql.Timestamp.valueOf(dataF.atStartOfDay()));
+                    Long total = query.getSingleResult();
 
-            if (total == 0) {
-                JOptionPane.showMessageDialog(null,
-                        "Nenhuma Ordem de Serviço encontrada no período selecionado.",
-                        "Aviso", JOptionPane.INFORMATION_MESSAGE);
-                return;
+                    if (total == 0) {
+                        JOptionPane.showMessageDialog(null,
+                                "Nenhuma Ordem de Serviço encontrada no período selecionado.",
+                                "Aviso", JOptionPane.INFORMATION_MESSAGE);
+                        return;
+                    }
+
+                    // Carrega o relatório do resources
+                    localArquivo = GerarRelatorio.class.getResourceAsStream(caminho);
+
+                    if (localArquivo == null) {
+                        throw new RuntimeException("Arquivo de relatório não encontrado no caminho: " + caminho);
+                    }
+
+                    // Compila o .jrxml
+                    JasperReport report = JasperCompileManager.compileReport(localArquivo);
+
+                    // Cria os parâmetros do relatório
+                    Map<String, Object> parametros = new HashMap<>();
+                    parametros.put("dataInicio", java.sql.Date.valueOf(dataI));
+                    parametros.put("dataFim", java.sql.Date.valueOf(dataF));
+
+                    // Preenche o relatório
+                    JasperPrint print = JasperFillManager.fillReport(report, parametros, conexao.getConnection());
+
+                    // Exporta o PDF
+                    JasperExportManager.exportReportToPdfFile(print, nomeArquivoSaida);
+                    JOptionPane.showMessageDialog(null, "✅ Relatório gerado com sucesso!");
+
+                    System.out.println("✅ PDF salvo em: " + nomeArquivoSaida);
+                    break;
+
+                case "Clientes":
+                    // Converte as datas do JTextField
+                    DateTimeFormatter formatador2 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                    LocalDate dataI2 = LocalDate.parse(dataInicio.getText(), formatador2);
+                    LocalDate dataF2 = LocalDate.parse(dataFim.getText(), formatador2);
+
+
+                    // Confere se há OS no período
+                    String jpql2 = "SELECT COUNT(o) FROM Clientes  o WHERE o.data_cadastro BETWEEN :dataI AND :dataF";
+                    TypedQuery<Long> query2 = conexao.em.createQuery(jpql2, Long.class);
+                    query2.setParameter("dataI", java.sql.Date.valueOf(dataI2));
+                    query2.setParameter("dataF", java.sql.Date.valueOf(dataF2));
+                    Long total2 = query2.getSingleResult();
+
+                    if (total2 == 0) {
+                        JOptionPane.showMessageDialog(null,
+                                "Nenhuma Ordem de Serviço encontrada no período selecionado.",
+                                "Aviso", JOptionPane.INFORMATION_MESSAGE);
+                        return;
+                    }
+
+                    // Carrega o relatório do resources
+                    localArquivo = GerarRelatorio.class.getResourceAsStream(caminho);
+
+                    if (localArquivo == null) {
+                        throw new RuntimeException("Arquivo de relatório não encontrado no caminho: " + caminho);
+                    }
+
+                    // Compila o .jrxml
+                    JasperReport report2 = JasperCompileManager.compileReport(localArquivo);
+
+                    // Cria os parâmetros do relatório
+                    Map<String, Object> parametros2 = new HashMap<>();
+                    parametros2.put("dataInicio", java.sql.Date.valueOf(dataI2));
+                    parametros2.put("dataFim", java.sql.Date.valueOf(dataF2));
+
+                    // Preenche o relatório
+                    JasperPrint print2 = JasperFillManager.fillReport(report2, parametros2, conexao.getConnection());
+
+                    // Exporta o PDF
+                    JasperExportManager.exportReportToPdfFile(print2, nomeArquivoSaida);
+                    JOptionPane.showMessageDialog(null, "✅ Relatório gerado com sucesso!");
+
+                    System.out.println("✅ PDF salvo em: " + nomeArquivoSaida);
+                    break;
+
             }
 
-            // Carrega o relatório do resources
-            localArquivo = GerarRelatorio.class.getResourceAsStream(caminho);
-
-            if (localArquivo == null) {
-                throw new RuntimeException("Arquivo de relatório não encontrado no caminho: " + caminho);
-            }
-
-            // Compila o .jrxml
-            JasperReport report = JasperCompileManager.compileReport(localArquivo);
-
-            // Cria os parâmetros do relatório
-            Map<String, Object> parametros = new HashMap<>();
-            parametros.put("dataInicio", java.sql.Date.valueOf(dataI));
-            parametros.put("dataFim", java.sql.Date.valueOf(dataF));
-
-            // Preenche o relatório
-            JasperPrint print = JasperFillManager.fillReport(report, parametros, conexao.getConnection());
-
-            // Exporta o PDF
-            JasperExportManager.exportReportToPdfFile(print, nomeArquivoSaida);
-            JOptionPane.showMessageDialog(null, "✅ Relatório gerado com sucesso!");
-
-            System.out.println("✅ PDF salvo em: " + nomeArquivoSaida);
 
         } catch (Exception e) {
             e.printStackTrace();
