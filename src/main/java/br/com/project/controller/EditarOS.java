ef12a4dc0cda9fa810e07ffcfbb611326e77262b
venicio da silva ferreira <veniciodasilvaferreira9@gmail.com>
2025-11-23 13:39:14 -0300

    feat - 1 A requisição e verificaçao das ordens estava incorreta, correção adicionando uma variavel para capturar a ordem e em seguida verifica se existe

--- src/main/java/br/com/project/controller/EditarOS.java
+++ src/main/java/br/com/project/controller/EditarOS.java
@@ -1,6 +1,5 @@
 package br.com.project.controller;
 
-
 import br.com.project.dao.Dao;
 import br.com.project.domain.OrdensDeServico;
 
@@ -8,34 +7,45 @@ import javax.swing.*;
 import java.sql.Timestamp;
 
 public class EditarOS {
-    public OrdensDeServico Editar(String NOS, String data, String equipamento, String servico, String defeito, String tec, JRadioButton Tipo, String Status, String valor) {
+    public OrdensDeServico Editar(JTextField NOS, JTextField data, JTextField equipamento, JTextField servico, JTextField defeito, JTextField tec, JTextField Tipo,  JComboBox<String> Status, JTextField valor) {
         Dao conexao = new Dao();
         conexao.IniciarConexao();
         OrdensDeServico Update = new OrdensDeServico();
         try {
-            Update.setData_os(new Timestamp(System.currentTimeMillis()));
-            Update.setEquipamento(equipamento);
-            Update.setDefeito(defeito);
-            Update.setServico(servico);
-            Update.setTecnico(tec);
-            Update.setTipo(Tipo.getText());
-            Update.setStatus(Status);
-            Update.setValor(Double.parseDouble(valor));
-            Update = conexao.em.find(OrdensDeServico.class, NOS);
-            if (Update == null) {
-                System.out.println("Nenhum ordem de serviço encontrada");
-                return null;
-            } else {
+        //1 captura o numero da ordem de serviço
+        int os = Integer.parseInt(NOS.getText());
 
+        //2 consulta no banco de dados a ordem
+        Update = conexao.em.find(OrdensDeServico.class, os);
+
+        //3 verifica se a ordem existe no banco de dados
+        if (Update == null) {
+            System.out.println("Nenhum ordem de serviço encontrada");
+            return null;
+        } else {
+
+                // 4 muda os dados
+                Update.setData_os(new Timestamp(System.currentTimeMillis()));
+                Update.setEquipamento(equipamento.getText());
+                Update.setDefeito(defeito.getText());
+                Update.setServico(servico.getText());
+                Update.setTecnico(tec.getText());
+                Update.setTipo(Tipo.getText());
+                Update.setStatus(String.valueOf(Status));
+                Update.setValor(Double.parseDouble(valor.getText()));
+
+                //todo corrigir o radiobutton status
+
+                //5- att no banco
                 conexao.em.getTransaction().begin();
                 conexao.em.merge(Update);
                 conexao.em.getTransaction().commit();
                 System.out.println("Ordem atualizada com sucesso");
 
             }
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Erro de conexao com banco de dados" + e.getLocalizedMessage());
-        } finally {
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        } finally{
             conexao.FecharConexao();
         }
         return Update;
