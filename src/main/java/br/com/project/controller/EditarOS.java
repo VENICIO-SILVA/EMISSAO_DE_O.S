93f447002b490790382754cdbe1edafda256a3c5
venicio da silva ferreira <veniciodasilvaferreira9@gmail.com>
2025-11-23 10:33:33 -0300

    feat - Update de dados das ordens de serviço

--- src/main/java/br/com/project/controller/EditarOS.java
+++ src/main/java/br/com/project/controller/EditarOS.java
@@ -1,8 +1,43 @@
 package br.com.project.controller;
 
-public class EditarOS {
 
-    public EditarOS Editar(){
-        return null;
+import br.com.project.dao.Dao;
+import br.com.project.domain.OrdensDeServico;
+
+import javax.swing.*;
+import java.sql.Timestamp;
+
+public class EditarOS {
+    public OrdensDeServico Editar(String NOS, String data, String equipamento, String servico, String defeito, String tec, JRadioButton Tipo, String Status, String valor) {
+        Dao conexao = new Dao();
+        conexao.IniciarConexao();
+        OrdensDeServico Update = new OrdensDeServico();
+        try {
+            Update.setData_os(new Timestamp(System.currentTimeMillis()));
+            Update.setEquipamento(equipamento);
+            Update.setDefeito(defeito);
+            Update.setServico(servico);
+            Update.setTecnico(tec);
+            Update.setTipo(Tipo.getText());
+            Update.setStatus(Status);
+            Update.setValor(Double.parseDouble(valor));
+            Update = conexao.em.find(OrdensDeServico.class, NOS);
+            if (Update == null) {
+                System.out.println("Nenhum ordem de serviço encontrada");
+                return null;
+            } else {
+
+                conexao.em.getTransaction().begin();
+                conexao.em.merge(Update);
+                conexao.em.getTransaction().commit();
+                System.out.println("Ordem atualizada com sucesso");
+
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro de conexao com banco de dados" + e.getLocalizedMessage());
+        } finally {
+            conexao.FecharConexao();
+        }
+        return Update;
     }
 }
